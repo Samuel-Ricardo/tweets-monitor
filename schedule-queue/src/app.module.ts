7d1160feab06a3a76045ffe5bc091fa31afec459
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-07 18:03:03 -0300

    [:gear:] setup: modules (main)

--- schedule-queue/src/app.module.ts
+++ schedule-queue/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bull';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
