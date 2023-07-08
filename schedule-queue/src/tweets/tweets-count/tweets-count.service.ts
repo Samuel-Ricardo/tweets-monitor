51196bdcfdb960af306de5735752805094083b32
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-07 23:44:56 -0300

    [:sparkles:]  | create: service - count (tweet)

--- schedule-queue/src/tweets/tweets-count/tweets-count.service.ts
+++ schedule-queue/src/tweets/tweets-count/tweets-count.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Tweet } from '../entities/tweet.entity';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
 
 @Injectable()
-export class TweetsCountService {}
+export class TweetsCountService {
+  private limit = 10;
+
+  constructor(
+    @Inject(Tweet)
+    private tweetModel: typeof Tweet,
+
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+
+    @InjectQueue('emails')
+    private emailsQueue: Queue,
+  ) {}
+}
