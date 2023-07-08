cbc48bd592e385ba24f926dba71a2a520489fff0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-07 23:07:02 -0300

    [:sparkles:]  | create: entity - Tweet

--- schedule-queue/src/tweets/entities/tweet.entity.ts
+++ schedule-queue/src/tweets/entities/tweet.entity.ts
@@ -1,1 +1,7 @@
-export class Tweet {}
+import { Table, Model, Column } from 'sequelize-typescript';
+
+@Table({ tableName: 'tweets' })
+export class Tweet extends Model {
+  @Column
+  text: string;
+}
