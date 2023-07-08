20577926acbd3f3d49431b048440b57522b9bc2c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-07 23:16:40 -0300

    [:sparkles:]  | update: module - Tweet

--- schedule-queue/src/tweets/tweets.module.ts
+++ schedule-queue/src/tweets/tweets.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { TweetsCountService } from './tweets-count/tweets-count.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Tweet } from './entities/tweet.entity';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
+  imports: [
+    CacheModule.register(),
+    SequelizeModule.forFeature([Tweet]),
+    BullModule.registerQueue({ name: 'emails' }),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService, TweetsCountService],
 })
