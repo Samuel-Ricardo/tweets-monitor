9674241004d30156365d54fda951e4e5ab4419d0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-08 00:14:25 -0300

    [:sparkles:]  | create: fun - countTweets (CountTweetsService)

--- schedule-queue/src/tweets/tweets-count/tweets-count.service.ts
+++ schedule-queue/src/tweets/tweets-count/tweets-count.service.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Tweet } from '../entities/tweet.entity';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import { Interval } from '@nestjs/schedule';
 
 @Injectable()
 export class TweetsCountService {
@@ -18,4 +20,24 @@ export class TweetsCountService {
     @InjectQueue('emails')
     private emailsQueue: Queue,
   ) {}
+
+  @Interval(5000)
+  async countTweets() {
+    console.log('Searching Tweets...');
+
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+
+    offset = offset === undefined ? 0 : offset;
+
+    console.log(` Offset: ${offset}`);
+
+    const tweets = await this.tweetModel.findAll({
+      offset,
+      limit: this.limit,
+    });
+
+    console.log(`| ${tweets.length} Tweets Found`);
+
+    this.emailsQueue.add({ tweets: tweets.map((t) => t.toJSON()) });
+  }
 }
